71aec9a79c0d28d99dbc0a7622cc32e10c163263
gmeghajain <gmeghajain@gmail.com>
2020-10-09 19:24:49 +0530

    Conditional calculation for wages

--- EmployeeWageComputationProblem.sh
+++ EmployeeWageComputationProblem.sh
@@ -1,12 +1,32 @@
 #! /bin/bash -x
 
-#Calculate Employee wage for a month (20 days) part time employee
+#Calculate Employee wage for a month (max 20 Working days) part time employee
 #asuming wage per hour is 20 and full day hours are 4
 
 month=20
 WagePerHour=20
 PartTimeDayHours=4
 FullTimeDayHours=8
+totalWorkingHoursFT=0
+totalWorkingHoursPT=0
 
-echo "Part time Employee Wage for a month = " $(( $WagePerHour * $PartTimeDayHours * $month))
-echo "Full time Employee Wage for a month = " $(( $WagePerHour * $FullTimeDayHours * $month))
+totalWorkingDays=1
+
+while [[ totalWorkingHoursFT -le 100 && totalWorkingDays -le 20 ]]
+do
+	FullTimeEmployeeWage=$((  $WagePerHour * $FullTimeDayHours * $totalWorkingDays ))
+	totalWorkingDays++
+	totalWorkingHoursFT+=8
+	
+done
+
+while [[ totalWorkingHoursPT -le 100 && totalWorkingDays -le 20 ]]
+do
+	PartTimeEmployeeWage=$((  $WagePerHour * $PartTimeDayHours * $totalWorkingDays ))
+        totalWorkingHoursPT+=4
+	totalWorkingDays++
+
+done
+
+echo "Part time Employee Wage" $PartTimeEmployeeWage
+echo "Full time Employee Wage" $FullTimeEmployeeWage
