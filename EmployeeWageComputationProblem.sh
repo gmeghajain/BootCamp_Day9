148e50cbf401779bddb1aacf750323ad75868e33
gmeghajain <gmeghajain@gmail.com>
2020-10-09 18:50:40 +0530

    Monthly Wage UC

--- EmployeeWageComputationProblem.sh
+++ EmployeeWageComputationProblem.sh
@@ -1,11 +1,12 @@
 #! /bin/bash -x
 
-#Calculate Daily employee wage for part time employee
+#Calculate Employee wage for a month (20 days) part time employee
 #asuming wage per hour is 20 and full day hours are 4
 
-
+month=20
 WagePerHour=20
 PartTimeDayHours=4
+FullTimeDayHours=8
 
-echo "Part time Employee Daily Wage = " $(( $WagePerHour * $PartTimeDayHours ))
-
+echo "Part time Employee Wage for a month = " $(( $WagePerHour * $PartTimeDayHours * $month))
+echo "Full time Employee Wage for a month = " $(( $WagePerHour * $FullTimeDayHours * $month))
