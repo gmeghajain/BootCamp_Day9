6a9c53dc49bd280758057ede088161aa87f93244
gmeghajain <gmeghajain@gmail.com>
2020-10-09 18:40:24 +0530

    Switch Case UC

--- EmployeeWageComputationProblem.sh
+++ EmployeeWageComputationProblem.sh
@@ -1,11 +1,28 @@
 #! /bin/bash -x
 
-#Calculate Daily employee wage for part time employee
-#asuming wage per hour is 20 and full day hours are 4
+#Calculate Daily employee wage for Full time and part time employee
+#asuming wage per hour is 20 and full day hours for full time and
+# part time employee are 8 and 4 respectively
 
+isFullTime=0
+isPartTime=1
 
 WagePerHour=20
 PartTimeDayHours=4
+FullTimeDayHours=8
 
-echo "Part time Employee Daily Wage = " $(( $WagePerHour * $PartTimeDayHours ))
+x=$((RANDOM%2))
+
+case $x in
+	0)
+		echo "Daily Employee Wage for Full Time Employee is:" $(( $WagePerHour * $FullTimeDayHours ))
+	;;
+	1)
+		echo "Daily Employee Wage for Part Time Employee is:" $(( $WagePerHour * $PartTimeDayHours ))
+	;;
+	*)
+	echo "Employee not Found"
+	;;
+esac
+
 
