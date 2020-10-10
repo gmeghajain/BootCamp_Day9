c6e98d272e25bebeb9bebbcf39ba99be04c72226
gmeghajain <gmeghajain@gmail.com>
2020-10-10 18:52:18 +0530

    Either days less than 20 or Working hours less than 100

--- EmployeeWageComputationProblem.sh
+++ EmployeeWageComputationProblem.sh
@@ -10,23 +10,24 @@ FullTimeDayHours=8
 totalWorkingHoursFT=0
 totalWorkingHoursPT=0
 
-totalWorkingDays=1
+totalWorkingDaysFT=1
 
-while [[ totalWorkingHoursFT -le 100 && totalWorkingDays -le 20 ]]
+while [[ $totalWorkingHoursFT -le 100 && $totalWorkingDaysFT -le 20 ]]
 do
-	FullTimeEmployeeWage=$((  $WagePerHour * $FullTimeDayHours * $totalWorkingDays ))
-	totalWorkingDays++
+	FullTimeEmployeeWage=$(( $WagePerHour * $FullTimeDayHours * $totalWorkingDaysFT ))
+	totalWorkingDaysFT+=1
 	totalWorkingHoursFT+=8
 	
 done
 
-while [[ totalWorkingHoursPT -le 100 && totalWorkingDays -le 20 ]]
+totalWorkingDaysPT=1
+while [[ $totalWorkingHoursPT -le 100 && $totalWorkingDaysPT -le 20 ]]
 do
-	PartTimeEmployeeWage=$((  $WagePerHour * $PartTimeDayHours * $totalWorkingDays ))
+	PartTimeEmployeeWage=$((  $WagePerHour * $PartTimeDayHours * $totalWorkingDaysPT ))
         totalWorkingHoursPT+=4
-	totalWorkingDays++
+	totalWorkingDaysPT+=1
 
 done
 
-echo "Part time Employee Wage" $PartTimeEmployeeWage
-echo "Full time Employee Wage" $FullTimeEmployeeWage
+echo "Part time Employee Wage = " $PartTimeEmployeeWage
+echo "Full time Employee Wage = " $FullTimeEmployeeWage
